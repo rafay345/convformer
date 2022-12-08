008df30cc87e249bc07844b3f630e4c603427c04
samuelwheeler <swwheeler@uchicago.edu>
2022-12-08 12:35:08 -0600

    Update convformer_cifar10.py

--- convformer_cifar10.py
+++ convformer_cifar10.py
@@ -62,7 +62,7 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-batch_size = 128
+batch_size = 256
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
