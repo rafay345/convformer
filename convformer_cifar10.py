dc510a72763f3b1a6e0cfc64b76791606ea9d574
samuelwheeler <swwheeler@uchicago.edu>
2022-12-08 09:55:04 -0600

    Update convformer_cifar10.py

--- convformer_cifar10.py
+++ convformer_cifar10.py
@@ -49,13 +49,9 @@ get on gpu
 '''
 
 
-gpu_info = !nvidia-smi
-gpu_info = '\n'.join(gpu_info)
-if gpu_info.find('failed') >= 0:
-    print('Not connected to a GPU')
-else:
-    print(gpu_info)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
 
 '''
 load data
